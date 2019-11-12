b8fc02cf63dedea268609573e90d2ea00ea4c691
zhaowei <zhaowei@lvmama.com>
2019-11-12 17:46:41 +0800

    hook

--- app/src/main/java/com/android/constrainlayoutdemo/delegate/Event.java
+++ app/src/main/java/com/android/constrainlayoutdemo/delegate/Event.java
@@ -1,5 +1,9 @@
 package com.android.constrainlayoutdemo.delegate;
 
 public class Event {
+ private Object object;
+ private String methodName;
+ private Object[] params;
+ private Class[] paramTypes;
 
 }
