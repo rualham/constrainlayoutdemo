5b8cda0aaf0ca8d253654b382e044de1173283c0
zhaowei <zhaowei@lvmama.com>
2019-11-13 14:07:28 +0800

    hook

--- app/src/main/java/com/android/constrainlayoutdemo/delegate/Event.java
+++ app/src/main/java/com/android/constrainlayoutdemo/delegate/Event.java
@@ -1,9 +1,44 @@
 package com.android.constrainlayoutdemo.delegate;
 
-public class Event {
- private Object object;
- private String methodName;
- private Object[] params;
- private Class[] paramTypes;
+import java.lang.reflect.Method;
 
+public class Event {
+    private Object object;
+    private String methodName;
+    private Object[] params;
+    private Class[] paramTypes;
+
+    public Event() {
+    }
+
+    public Event(Object object, String methodName, Object... args) {
+        this.object = object;
+        this.methodName = methodName;
+        this.params = args;
+        contractParamTypes(this.params);
+    }
+
+    //根据参数数组生成参数类型数组
+    private void contractParamTypes(Object[] params) {
+        this.paramTypes = new Class[params.length];
+        for (int i = 0; i < params.length; i++) {
+            this.paramTypes[i] = params[i].getClass();
+        }
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setParamTypes(Class[] paramTypes) {
+        this.paramTypes = paramTypes;
+    }
+
+    public void invoke() throws Exception {
+        Method method=object.getClass().getMethod(this.methodName,this.paramTypes);
+        if(null==method){
+            return;
+        }
+        method.invoke(this.getObject(),this.params);
+    }
 }
---
+++ app/src/main/java/com/android/constrainlayoutdemo/delegate/EventHandler.java
@@ -0,0 +1,25 @@
+package com.android.constrainlayoutdemo.delegate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventHandler {
+
+    private List<Event> objects;
+
+    public EventHandler() {
+        this.objects = new ArrayList<>();
+    }
+
+    //添加某个对象要执行的事件，及需要的参数
+    public void addEvent(Object object, String methodName, Object... args) {
+        objects.add(new Event(object, methodName, args));
+    }
+
+    //通知所有的对象执行指定的事件
+    public void notifyX() throws Exception {
+        for (Event e : objects) {
+            e.invoke();
+        }
+    }
+}
